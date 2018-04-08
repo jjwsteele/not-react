65fa0829178c9ddf9af67684a6f1c16881ade15f
Jordan Steele <jjwsteele@gmail.com>
2018-04-08 19:53:14 +1000

    Update demo to make task descriptions togglable

--- demo/TodoListItem.js
+++ demo/TodoListItem.js
@@ -1,14 +1,33 @@
-import NotReact from '../src/NotReact'
+import NotReact, { Component } from '../src/NotReact'
 
-const TodoListItem = ({ task }) => NotReact.createElement(
-  'li',
-  null,
-  task.name,
-  NotReact.createElement(
-    'ul',
-    null,
-    NotReact.createElement('li', null, task.description),
-  )
-)
+class TodoListItem extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isExpanded: false,
+    }
+
+    this.toggleExpanded = this.toggleExpanded.bind(this)
+  }
+
+  toggleExpanded() {
+    this.setState({
+      isExpanded: !this.state.isExpanded,
+    })
+  }
+
+  render() {
+    return NotReact.createElement(
+      'li',
+      { onClick: this.toggleExpanded },
+      this.props.task.name,
+      this.state.isExpanded && NotReact.createElement(
+        'ul',
+        null,
+        NotReact.createElement('li', null, this.props.task.description),
+      )
+    )
+  }
+}
 
 export default TodoListItem
